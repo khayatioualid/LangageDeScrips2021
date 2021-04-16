64bd38ffc22077f0cb155171a270d2ca2baefe05
khayati <oualid.khayati@gmail.com>
2021-04-16 15:09:39 +0100

    correction

--- ListeCreation.py
+++ ListeCreation.py
@@ -1,8 +1,16 @@
+def createList(*T):
+    L=[0]*T[0]
+    if (len(T) >1) :
+        for i in range(len(L)):
+            L[i]=createList(*T[1:])
+    return L
 
 
-L1=createList(4)
+L1 = createList(4)
 print(L1)   # ==> [0, 0, 0, 0]
-L2=createList(2, 4)
+L2 = createList(2, 4)
 print(L2)   # ==> [[0, 0, 0, 0], [0, 0, 0, 0]]
-L3=createList(3, 2, 4)
-print(L2)   # ==> [[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
+L3 = createList(3, 2, 4)
+print(L3)   # ==> [[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
+L4 = createList(2, 3, 2, 4)
+print(L4)

d6cab1b4cab01201cc9cae5bb007712fc157643a
khayati <oualid.khayati@gmail.com>
2021-05-07 13:26:07 +0100

    comprehension list exercice

--- CompehensionList.py
+++ CompehensionList.py
@@ -1,5 +1,5 @@
 from functools import reduce
-
+"""
 L=[x for x in range(100)]
 Limpaire=[x for x in range(100) if x%2!=0]
 #L=[expression en fonction de x pour x dans une sequence eventuellement vérifiant une certaine condition]
@@ -11,10 +11,10 @@ s=reduce(lambda x,y:x+y,L)
 p=reduce(lambda x,y:x*y,[x for x in range(100) if x%2!=0])
 print(s)
 print(p)
-"""
-L1=list(map(lambda x:2*x,L))
-print(L1)
-"""
+
+#L1=list(map(lambda x:2*x,L))
+#print(L1)
+
 m=map(lambda x:2*x,L)
 for v in m:
     print(v)
@@ -24,3 +24,18 @@ L2=[10,20,30,40]
 L3=[100,200,300,400]
 LR=list(zip(L1,L2,L3))
 print(LR)
+"""
+#Exercice
+#ecrire en une ligne l'expression permettant le calcul de la somme de deux vecteurs
+L1=[1,2,3,4]
+L2=[10,20,30,40]
+
+#solution 1
+A=list(map(lambda x:x[0]+x[1], zip(L1,L2)))
+print(A)
+print([x + y for x, y in zip(L1, L2)])
+#solution2
+print(list(map(lambda x,y: x+y,L1,L2)))
+#ecrire en une ligne l'expression permettant le calcul de leproduit de deux vecteurs
+
+#ecrire en une ligne l'expression permettant le calcul de la puissance de deux vecteurs
